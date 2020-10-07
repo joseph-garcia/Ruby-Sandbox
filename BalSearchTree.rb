a6c1069dee55c44ab7b73e56bf7fcdf5dfb8fee4
Joseph Garcia <robhaywood12@gmail.com>
2020-10-07 16:40:27 -0500

    delete no child & 1 child
    functions complete

--- BalSearchTree.rb
+++ BalSearchTree.rb
@@ -1,27 +1,19 @@
-
-# initialize start = 0, end = array.length - 1
-# mid = (start + end) / 2
-# create a tree node with mid as root (lets call it A)
-# recursively do following steps:
-# calculate mid of left subarray and make it root of left subtree of A
-# calculate mid of right subarray and make it root of right subtree of A
-
 class Node
     attr_accessor :root, :left, :right
     
     def initialize(root)
         @root = root
     end
+
 end
 
 class Tree
+    @parentNode
+    #traverse left inits parent node
     attr_accessor :root
+
     def initialize(array)
         @array = array
-    end
-
-    def getRoot()
-        return @root
     end
 
     # create tree method
@@ -57,16 +49,19 @@ class Tree
                 insert_node(node_to_add, rootNode.left)
             end
         else
+            puts node_to_add
+            puts rootNode.root
             puts "Duplicate value. No new node added."
         end
+
+        return rootNode
 
     end
 
     def getChildStatus(rootNode)
         if rootNode.left.nil? && rootNode.right.nil?
             return "No children"
-        elsif !rootNode.left.nil? && !rootNode.left.nil?
-            puts rootNode.inspect
+        elsif (!rootNode.left.nil? && !rootNode.right.nil?)
             return "Two children"
         else
             return "One child"
@@ -74,46 +69,23 @@ class Tree
         puts "Error in getChildStatus"
     end
 
+    def traverseLeft(root_node)
+        @parentNode = root_node
+        if !root_node.left.nil?
+            traverseLeft(root_node.left)
+        else
+            return root_node
+        end
+    end
+
+    def switchNodes(old_node, new_node)
+        temp = old_node
+        old_node = new_node
+        new_node = temp
+    end
+
     def delete_node(node_to_delete, rootNode)
-        @parentNode
-        # 3 cases:
-        # delete a leaf if root has no children
-        def deleteNoChildren(rootNode)
-            rootNode.root = nil
-            @parentNode.right = nil
-            @parentNode.left = nil
-            puts "No children. Deleting"
-            return rootNode
-        end
-        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "No children"            
-            return deleteNoChildren(rootNode)
-        end
-
-        # delete a node w/ one child - have child take over parent
-        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "One child"
-            if rootNode.right.nil?
-                rootNode = rootNode.left
-                rootNode.left = nil
-            else
-                rootNode = rootNode.right
-                rootNode.right = nil
-            end
-            puts "One child."
-            return
-        end
-
-        # delete a node w/ 2 children - find the next highest # by
-        # visiting the left-most child of the right-child
-        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "Two children"
-            puts "Two childrenino."
-            return
-        end
-        # switch the left-most leaf with the root node
-        # if it isn't a leaf eg has a right child, then switch
-        # like normal but have child take over parent like in
-        # above function
-
-        # node_to_delete != rootNode, keep looking
+        # look for node to delete
         if node_to_delete != rootNode.root
             @parentNode = rootNode
             if node_to_delete > rootNode.root
@@ -121,6 +93,83 @@ class Tree
             elsif node_to_delete < rootNode.root
                 delete_node(node_to_delete, rootNode.left)
             end
+        end
+
+        # 3 cases of deletion:
+        #1. Delete a leaf if it has no children
+        def deleteNodeNoChild(node_to_delete)
+            # delete parent reference
+            if @parentNode.right == node_to_delete
+                @parentNode.right = nil
+            elsif @parentNode.left == node_to_delete
+                @parentNode.left = nil
+            end
+            return
+        end
+
+        #2. Delete a leaf if it has one child
+        def deleteOneChild(node_to_delete)
+            # Get a reference to node marked for deletion
+            if @parentNode.right == node_to_delete
+                upperNode = @parentNode.right
+            elsif @parentNode.left == node_to_delete
+                upperNode = @parentNode.left
+            end
+
+            # Get a reference to replacement node
+            lowerNode = upperNode.right.nil? ? upperNode.left : upperNode.right
+
+            #Switch values & unhook old node
+            if upperNode.right == lowerNode
+                tmp = lowerNode
+                upperNode.right = nil
+                upperNode.root = tmp.root
+                upperNode = tmp
+            elsif upperNode.left == lowerNode
+                tmp = lowerNode
+                upperNode.left = nil
+                upperNode.root = tmp.root
+                upperNode = tmp
+            else
+                puts "Error in deleteOneChild"
+            end
+
+            return
+        end
+
+        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "No children"            
+            return deleteNodeNoChild(rootNode)
+        end
+
+        # delete a node w/ one child - have child take over parent
+        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "One child"
+            return deleteOneChild(rootNode)        
+        end
+
+        # delete a node w/ 2 children - find the next highest # by
+        # visiting the left-most child of the right-child
+        if node_to_delete == rootNode.root && getChildStatus(rootNode) == "Two children"
+            #puts "Parent node is #{@parentNode.inspect}"
+            # go down one right-subtree
+            rightTreeNode = rootNode.right
+            # go to the furthest left you can go
+            leftMostNode = traverseLeft(rightTreeNode)
+            # if the left-most leaf has no right child, switch leaf with rootNode
+            # then delete leaf
+            if leftMostNode.right.nil?
+                rootNode.root = leftMostNode.root
+
+                deleteNodeNoChild(leftMostNode, @parentNode)
+                puts rootNode.inspect
+                puts
+                puts leftMostNode.inspect
+            else
+                #switch leaf with rootNode
+                rootNode.root = leftMostNode.root
+                #delete rootNode with deleteOneChild
+                deleteOneChild(leftMostNode, @parentNode)
+            end
+            return rootNode
         end
 
 
@@ -135,7 +184,7 @@ def pretty_print(node = @root, prefix = '', is_left = true)
 end
 
 # sort unsorted array
-initialArray = [1,2,3,4,5,10,11,12]
+initialArray = [2,6,14,12,18,20,24,36,48,52,70,5,8,44,1,3,99,4,25,31,26]
 sortedArray = initialArray.sort.uniq
 
 # create BST from sorted array
@@ -143,14 +192,11 @@ bsTree = Tree.new(sortedArray)
 bsTree.create_tree(sortedArray, 0, sortedArray.length-1)
 
 # insert node
-#bsTree.insert_node(11, bsTree.getRoot())
+bsTree.insert_node(13, bsTree.root)
 
-#delete node
-#bsTree.delete_node(11, bsTree.getRoot())
+#delete node test
+bsTree.delete_node(26, bsTree.root)
 
-bsTree.delete_node(11, bsTree.getRoot())
 
 # print tree
-pretty_print(bsTree.getRoot())
-
-puts bsTree.inspect
+pretty_print(bsTree.root)
